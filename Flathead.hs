37375af4b847ea95eef15d70b1451d48ce5cca1b
Gonzalo Rico <grico1974@gmail.com>
2016-02-18 09:49:26 +0100

    Better Zstring decoding

--- Flathead.hs
+++ Flathead.hs
@@ -6,18 +6,12 @@ import Types
 import Utility
 import qualified Story as Story
 import qualified Zstring as Zstring
-
+ 
 main = do 
    {
        bytes <- (BS.readFile "minizork.z3");
        let story = Story.load_Story bytes
-       in do
-       {
-           let zstring = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 0)
-               text = Zstring.display_Bytes story zstring
-           in printf "%s\n" text;
-           let zstring = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 4)
-               text = Zstring.display_Bytes story zstring
-           in printf "%s\n" text 
-        }      
+           zstringAddress = ZstringAddress 0xb106
+           text = Zstring.read_Story story zstringAddress in
+       printf "%s\n" text
    }   
--- Zstring.hs
+++ Zstring.hs
@@ -1,15 +1,31 @@
 module Zstring where
 
+import qualified Data.Char as Char (chr)
 import Text.Printf
 import Types
 import Utility
 import qualified Story as Story
 
+data StringState = Alphabet Int |
+                   Leading |
+                   Abbreviation AbbreviationNumber |
+                   Trailing Int
+                   deriving (Eq)
+
+alphabet0 = Alphabet 0
+alphabet1 = Alphabet 1
+alphabet2 = Alphabet 2
+abbrev0 = Abbreviation (AbbreviationNumber 0)
+abbrev32 = Abbreviation (AbbreviationNumber 32)
+abbrev64 = Abbreviation (AbbreviationNumber 64)
+
+alphabetTable :: [[Char]]
+alphabetTable = [ " ?????abcdefghijklmnopqrstuvwxyz",
+                  " ?????ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                  " ??????\n0123456789.,!?_#'\"/\\-:()" ]                
+
 abbreviationTableLength :: Int
 abbreviationTableLength = 96
-
-alphabetTable :: [Char]
-alphabetTable = "_?????abcdefghijklmnopqrstuvwxyz"
 
 uncompress_Address :: WordZstringAddress -> ZstringAddress
 uncompress_Address address =
@@ -33,25 +49,60 @@ abbreviation_ZstringAddress story number =
             compressedAddress = WordZstringAddress (Story.read_Word story abbreviationAddress)
         in uncompress_Address compressedAddress
 
+read_Story :: Story.Story -> ZstringAddress -> String
+read_Story story addr =
+   let ZstringAddress address = addr
+   in aux "" alphabet0 (WordAddress address) where
+      aux :: String -> StringState -> WordAddress -> String
+      aux acc state1 current_address =
+         let zcharBitSize = size5
+             word = Story.read_Word story current_address
+             isEnd = fetch_Bit bit15 word
+             zchar1 = Zchar (fetch_Bits bit14 size5 word)
+             zchar2 = Zchar (fetch_Bits bit9 size5 word)
+             zchar3 = Zchar (fetch_Bits bit4 size5 word)
+             (text1, state2) = process_Zchar zchar1 state1
+             (text2, state3) = process_Zchar zchar2 state2
+             (text3, stateNext) = process_Zchar zchar3 state3
+             newAcc = acc ++ text1 ++ text2 ++ text3
+         in
+            if isEnd == One then newAcc
+            else aux newAcc stateNext (increment_WordAddress current_address)
+         where
+            process_Zchar :: Zchar -> StringState -> (String, StringState)
+            process_Zchar zcharacter state =
+               let (Zchar zchar) = zcharacter in
+               case (zchar, state) of 
+                  (1, Alphabet _) -> ("", abbrev0)
+                  (2, Alphabet _) -> ("", abbrev32)
+                  (3, Alphabet _) -> ("", abbrev64)
+                  (4, Alphabet _) -> ("", alphabet1)
+                  (5, Alphabet _) -> ("", alphabet2)
+                  (6, Alphabet 2) -> ("", Leading)
+                  (_, Alphabet a) -> ([alphabetTable !! a !! zchar], alphabet0)
+                  (_, Abbreviation (AbbreviationNumber a)) ->
+                     let abbrv = AbbreviationNumber(a + zchar)
+                         addr = abbreviation_ZstringAddress story abbrv
+                         str = read_Story story addr
+                     in (str, alphabet0)
+                  (_, Leading) -> ("", Trailing zchar)
+                  (_, Trailing high) ->
+                     let s = [Char.chr (high * 32 + zchar)]
+                     in (s, alphabet0)  
+ 
 display_Bytes :: Story.Story -> ZstringAddress -> String
 display_Bytes story address =
    let ZstringAddress addr = address
    in aux (WordAddress addr) "" where
+      aux :: WordAddress -> String -> String
       aux current acc =
          let word = Story.read_Word story current
              isEnd = fetch_Bit bit15 word
              zchar1 = fetch_Bits bit14 size5 word
              zchar2 = fetch_Bits bit9 size5 word
              zchar3 = fetch_Bits bit4 size5 word
-             s = printf "%x %c %x %c %x %c "
-                        zchar1 (alphabetTable !! zchar1)
-                        zchar2 (alphabetTable !! zchar2)
-                        zchar3 (alphabetTable !! zchar3)
+             s = printf "%x %x %x " zchar1 zchar2 zchar3
              newAcc = acc ++ s
          in
             if isEnd == One then newAcc
             else aux (increment_WordAddress current) newAcc
-
-
-      
-   
