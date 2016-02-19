78a192944a0e0078120474707ff55375834b1581
Gonzalo Rico <grico1974@gmail.com>
2016-02-19 17:53:24 +0100

    Made file loading portable

--- Grue.hs
+++ Grue.hs
@@ -1,17 +1,19 @@
 module Main where
 
 import qualified Data.ByteString as BS
+import System.Environment
 import Text.Printf
 import Types
-import Utility
 import qualified Story as Story
 import qualified Zstring as Zstring
  
 main = do 
    {
-       bytes <- (BS.readFile "./minizork.z3");
+       path <- getExecutablePath;
+       progName <- getProgName;
+       bytes <- BS.readFile (take (length path - length progName) path ++ "minizork.z3");
        let story = Story.load_Story bytes
            zstringAddress = ZstringAddress 0xb106
            text = Zstring.read_Story story zstringAddress in
-       printf "%s\n" text
-   }   
+       printf "%s\n" text  
+   } 
