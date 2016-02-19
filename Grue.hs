d13ee6258ec1e73cdb6286652269e9f2a15d862a
Gonzalo Rico <grico1974@gmail.com>
2016-02-19 08:27:52 +0100

    Made path OS X compatible

--- Grue.hs
+++ Grue.hs
@@ -9,7 +9,7 @@ import qualified Zstring as Zstring
  
 main = do 
    {
-       bytes <- (BS.readFile "minizork.z3");
+       bytes <- (BS.readFile "./minizork.z3");
        let story = Story.load_Story bytes
            zstringAddress = ZstringAddress 0xb106
            text = Zstring.read_Story story zstringAddress in
