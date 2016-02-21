a36a11ce03ff6179eb0b6bfa3ed9045cf3c0aa10
Gonzalo Rico <grico1974@gmail.com>
2016-02-21 02:24:30 +0100

    Refactoring code

--- Grue.hs
+++ Grue.hs
@@ -5,15 +5,13 @@ import System.Environment
 import Text.Printf
 import Types
 import qualified Story as Story
-import qualified Zstring as Zstring
- 
-main = do 
-   {
-       path <- getExecutablePath;
-       progName <- getProgName;
-       bytes <- BS.readFile (take (length path - length progName) path ++ "minizork.z3");
+import qualified Zstring as Zstring 
+
+main = do
+       path <- getExecutablePath
+       progName <- getProgName
+       let storyPath = take (length path - length progName) path ++ "minizork.z3"
+       bytes <- BS.readFile storyPath
        let story = Story.load_Story bytes
-           zstringAddress = ZstringAddress 0xb106
-           text = Zstring.read_Story story zstringAddress in
-       printf "%s\n" text  
-   } 
+           text = Zstring.read_Story story $ ZstringAddress 0xb106
+       printf "%s\n" text
