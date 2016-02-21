2d2a82a7bd6a0a2989209c3b2112baf55a72be8b
Gonzalo Rico <grico1974@gmail.com>
2016-02-21 02:29:57 +0100

    Refactoring code

--- Grue.hs
+++ Grue.hs
@@ -8,19 +8,15 @@ import Utility
 import qualified Story as Story
 import qualified Zstring as Zstring
 
-main = do 
-   {
-       path <- getExecutablePath;
-       progName <- getProgName;
-       bytes <- BS.readFile (take (length path - length progName) path ++ "minizork.z3");
+main = do
+       path <- getExecutablePath
+       progName <- getProgName
+       let storyPath = take (length path - length progName) path ++ "minizork.z3"
+       bytes <- BS.readFile storyPath
        let story = Story.load_Story bytes
-       in do
-       {
-           let zstring = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 0)
-               text = Zstring.display_Bytes story zstring
-           in printf "%s\n" text;
-           let zstring = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 4)
-               text = Zstring.display_Bytes story zstring
-           in printf "%s\n" text 
-        }      
-   }   
+           zstring1 = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 0)
+           text1 = Zstring.display_Bytes story zstring1
+           zstring2 = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 4)
+           text2 = Zstring.display_Bytes story zstring2
+       printf "%s\n" text1
+       printf "%s\n" text2  
