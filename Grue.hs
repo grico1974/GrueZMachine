05217cef3f136ea8189a3185fae071fd758bf5a3
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:23:09 +0100

    Code refactoring

--- Grue.hs
+++ Grue.hs
@@ -2,15 +2,22 @@ module Main where
 
 import qualified Data.ByteString as BS
 import Text.Printf
+import System.Environment
 import Types
 import Utility
 import qualified Story as Story
 
-main = do {
-    bytes <- (BS.readFile "./minizork.z3");
-    let story = Story.load_Story bytes in
-    let versionAddress = ByteAddress 0 in
-    let version = Story.read_Byte story versionAddress in
-    printf "Version of file is: v%d\n" version
-  }
+main = 
+   do bytes <- open_Story_File "minizork.z3"
+      let story = Story.load_Story bytes
+      let versionAddress = ByteAddress 0
+      printf "Version of file is: v%d\n" $ Story.read_Byte story versionAddress
+  
+--Platform independent
+--Relative paths are not working as expected in OS X
+open_Story_File :: FilePath -> IO BS.ByteString
+open_Story_File fileName = 
+   do path <- getExecutablePath
+      progName <- getProgName
+      BS.readFile $ take (length path - length progName) path ++ fileName
   
