7792420a925acd741cf38e4209140f7ad3d9a7d4
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:46:13 +0100

    Code refactoring

--- Grue.hs
+++ Grue.hs
@@ -1,12 +1,14 @@
 module Main where
 
 import qualified Data.ByteString as BS
+import System.Environment
 import Text.Printf
 import System.Environment
 import Types
 import Utility
 import qualified Story as Story
 
+<<<<<<< HEAD
 main :: IO ()
 main = 
    do bytes <- open_Story_File "minizork.z3"
@@ -21,4 +23,15 @@ open_Story_File fileName =
    do path <- getExecutablePath
       progName <- getProgName
       BS.readFile $ take (length path - length progName) path ++ fileName
+=======
+main = do {
+    path <- getExecutablePath;
+    progName <- getProgName;
+    bytes <- BS.readFile (take (length path - length progName) path ++ "minizork.z3");
+    let story = Story.load_Story bytes in
+    let versionAddress = ByteAddress 0 in
+    let version = Story.read_Byte story versionAddress in
+    printf "Version of file is: v%d\n" version
+  }
+>>>>>>> origin/Part3
   
