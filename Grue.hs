de553b2f47143d6a93079c65673ec7e4f9f9d5be
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:20:59 +0100

    Code refactoring

--- Grue.hs
+++ Grue.hs
@@ -9,14 +9,19 @@ import qualified Story as Story
 import qualified Zstring as Zstring
 
 main = do
-       path <- getExecutablePath
-       progName <- getProgName
-       let storyPath = take (length path - length progName) path ++ "minizork.z3"
-       bytes <- BS.readFile storyPath
+       bytes <- open_Story_File "minizork.z3"
        let story = Story.load_Story bytes
            zstring1 = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 0)
            text1 = Zstring.display_Bytes story zstring1
            zstring2 = Zstring.abbreviation_ZstringAddress story (AbbreviationNumber 4)
            text2 = Zstring.display_Bytes story zstring2
        printf "%s\n" text1
-       printf "%s\n" text2  
+       printf "%s\n" text2
+
+--Platform independent
+--Relative paths are not working as expected in OS X
+open_Story_File :: FilePath -> IO BS.ByteString
+open_Story_File fileName = 
+   do path <- getExecutablePath
+      progName <- getProgName
+      BS.readFile $ take (length path - length progName) path ++ fileName
