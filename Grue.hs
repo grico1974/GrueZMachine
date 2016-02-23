5821a61530679c926fd296765fe5e42121077ee0
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:02:27 +0100

    Code refactoring

--- Grue.hs
+++ Grue.hs
@@ -9,12 +9,12 @@ import qualified Dictionary as Dictionary
 
 main :: IO ()
 main = do
-       bytes <- open_Story_File
+       bytes <- open_Story_File "minizork.z3"
        let story = Story.load_Story bytes
        printf "%s\n" $ Dictionary.display story
 
-open_Story_File :: IO BS.ByteString
-open_Story_File = do
+open_Story_File :: FilePath -> IO BS.ByteString
+open_Story_File fileName = do
                   path <- getExecutablePath
                   progName <- getProgName
-                  BS.readFile $ take (length path - length progName) path ++ "minizork.z3"
+                  BS.readFile $ take (length path - length progName) path ++ fileName
