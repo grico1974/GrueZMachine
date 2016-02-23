ea30c33bb3dac5e1f3878e7640566fa358689875
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:06:24 +0100

    Code refactoring

--- Grue.hs
+++ Grue.hs
@@ -8,13 +8,15 @@ import qualified Story as Story
 import qualified Dictionary as Dictionary 
 
 main :: IO ()
-main = do
-       bytes <- open_Story_File "minizork.z3"
-       let story = Story.load_Story bytes
-       printf "%s\n" $ Dictionary.display story
+main =
+   do bytes <- open_Story_File "minizork.z3"
+      let story = Story.load_Story bytes
+      printf "%s\n" $ Dictionary.display story
 
+--Platform independent
+--Relative paths are not working as expected in OS X
 open_Story_File :: FilePath -> IO BS.ByteString
-open_Story_File fileName = do
-                  path <- getExecutablePath
-                  progName <- getProgName
-                  BS.readFile $ take (length path - length progName) path ++ fileName
+open_Story_File fileName = 
+   do path <- getExecutablePath
+      progName <- getProgName
+      BS.readFile $ take (length path - length progName) path ++ fileName
