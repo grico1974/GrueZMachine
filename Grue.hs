8001b98aefb737c307277a7ec50f761e94ddb61c
Gonzalo Rico <grico1974@gmail.com>
2016-02-23 19:47:37 +0100

    Fixed unintended merge

--- Grue.hs
+++ Grue.hs
@@ -8,7 +8,6 @@ import Types
 import Utility
 import qualified Story as Story
 
-<<<<<<< HEAD
 main :: IO ()
 main = 
    do bytes <- open_Story_File "minizork.z3"
@@ -23,15 +22,3 @@ open_Story_File fileName =
    do path <- getExecutablePath
       progName <- getProgName
       BS.readFile $ take (length path - length progName) path ++ fileName
-=======
-main = do {
-    path <- getExecutablePath;
-    progName <- getProgName;
-    bytes <- BS.readFile (take (length path - length progName) path ++ "minizork.z3");
-    let story = Story.load_Story bytes in
-    let versionAddress = ByteAddress 0 in
-    let version = Story.read_Byte story versionAddress in
-    printf "Version of file is: v%d\n" version
-  }
->>>>>>> origin/Part3
-  
